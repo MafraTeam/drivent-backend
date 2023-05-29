47b5d7b7fdc99db1cd10f9d7d24468c2185524c5
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2023-05-29 11:17:42 -0300

    Inclusão do redis no repositório de room

--- src/repositories/room-repository/index.ts
+++ src/repositories/room-repository/index.ts
@@ -1,19 +1,30 @@
-import { prisma } from '@/config';
+import { Room } from '@prisma/client';
+import { prisma, redis } from '@/config';
+
+const key = 'room';
 
 async function findAllByHotelId(hotelId: number) {
-  return prisma.room.findMany({
-    where: {
-      hotelId,
-    },
-  });
+  const rooms: Room[] = JSON.parse(await redis.get(key));
+
+  if (rooms) return rooms.filter((r) => r.hotelId === hotelId);
+
+  const psRooms = await prisma.room.findMany();
+
+  await redis.set(key, JSON.stringify(psRooms));
+
+  return psRooms.filter((r) => r.hotelId === hotelId);
 }
 
 async function findById(roomId: number) {
-  return prisma.room.findFirst({
-    where: {
-      id: roomId,
-    },
-  });
+  const rooms: Room[] = JSON.parse(await redis.get(key));
+
+  if (rooms) return rooms.filter((r) => r.id === roomId)[0];
+
+  const psRooms = await prisma.room.findMany();
+
+  await redis.set(key, JSON.stringify(psRooms));
+
+  return psRooms.filter((r) => r.id === roomId)[0];
 }
 
 const roomRepository = {
